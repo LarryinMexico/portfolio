ed3af35210754f752ddf9f6ec6b98011ee1ac15f
SamChen <sam92.chen@gmail.com>
2025-08-14 18:51:19 +0800

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/portfolio/',
   plugins: [react()],
-})
+  base: '/portfolio/',  // 這裡的 'portfolio' 是你的 repo 名稱
+});
